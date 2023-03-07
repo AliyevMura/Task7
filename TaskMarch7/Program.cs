3189a6f052873ff60dd5772af7f8fd5b65b8a048
AliyevMura <7lvghl9@code.edu.az>
2023-03-07 11:28:19 +0400

    Initial commit

---
+++ TaskMarch7/Program.cs
@@ -0,0 +1,29 @@
+﻿//Task2
+//int number = 536, qaliq, sum = 0;
+//while (number > 0)
+//{
+//    qaliq = number % 10;
+//    sum = sum + qaliq;
+//    number = number / 10;
+//}
+//Console.WriteLine("reqemlerin cemi {0}", sum);
+
+
+
+//Task3
+//int num = int.Parse(Console.ReadLine());
+//int powerful = 1;
+//int result = 2;
+//while (powerful <= 100 && result != num)
+//{
+//    result *= 2;
+//    powerful++;
+//}
+//if (result == num)
+//{
+//    Console.WriteLine("2-nin quvvetidir");
+//}
+//else
+//{
+//    Console.WriteLine("2nin quvveti deyil");
+//}
